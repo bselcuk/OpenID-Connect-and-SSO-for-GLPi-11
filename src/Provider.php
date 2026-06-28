b99917dae824e07f3968d8c42afcf74392bac243
Macedit <macedit@MacNdk-MacBook-Pro.local>
2026-06-29 00:37:09 +0300

    fix: config_page yonlendirmesi ve provider ekleme yetkileri (canCreate) duzeltildi

--- src/Provider.php
+++ src/Provider.php
@@ -4,6 +4,12 @@ namespace GlpiPlugin\Openid;
 class Provider extends \CommonDBTM {
     static $rightname = 'config';
 
+    public static function canCreate() { return \Session::haveRight('config', UPDATE); }
+    public static function canView() { return \Session::haveRight('config', READ); }
+    public static function canUpdate() { return \Session::haveRight('config', UPDATE); }
+    public static function canDelete() { return \Session::haveRight('config', UPDATE); }
+    public static function canPurge() { return \Session::haveRight('config', UPDATE); }
+
     static function getTypeName($nb = 0) {
         return __('OpenID Provider', 'openid');
     }
